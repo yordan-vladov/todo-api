eab0ff2a1d928909f1ec84efb4bac07f364cd077
Yordan Vladov <jordan0vladov@gmail.com>
2024-05-26 11:53:14 +0300

    Initialized app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (_, res) => {
+    res.send("Hello, World");
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
